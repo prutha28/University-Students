598bab9cb8f9194785997883c77168098de5f36f
prutha28 <prutha.bits@gmail.com>
2017-02-06 00:44:34 -0500

    Added Endpoints for PUT, DELETE & POST.

--- studentportal/src/main/java/com/prutha/university/studentportal/resources/StudentResource.java
+++ studentportal/src/main/java/com/prutha/university/studentportal/resources/StudentResource.java
@@ -1,6 +1,10 @@
 package com.prutha.university.studentportal.resources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -8,5 +12,32 @@ import javax.ws.rs.core.MediaType;
 @Path("/students")
 public class StudentResource {
 	
-  
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getStudent(){
+//		ResponseBuilder rb = Response.accepted();
+//		return rb.build();
+		return "<html><body><h1>Hi Prutha!</h1></body></html>";
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
+	public String addStudent(){
+		return "A new Student added successfully.";
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
+	public String updateStudent(){
+		return "Student details updated successfully.";
+	}
+	
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_HTML)
+	public String deleteStudent(){
+		return "Student deleted successfully.";
+	}
 }
