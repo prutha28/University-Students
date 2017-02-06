e6d62f7f3a08ad163517fd1b2d4e47939b7c22c8
prutha28 <prutha.bits@gmail.com>
2017-02-06 00:50:29 -0500

    Added & Tested the GET Http endpoint by adding a PathParam.

--- studentportal/src/main/java/com/prutha/university/studentportal/resources/StudentResource.java
+++ studentportal/src/main/java/com/prutha/university/studentportal/resources/StudentResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,10 +15,12 @@ public class StudentResource {
 	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
-	public String getStudent(){
+	@Path("/{studentId}")
+	public String getStudent(@PathParam("studentId") 
+							String studentid){
 //		ResponseBuilder rb = Response.accepted();
 //		return rb.build();
-		return "<html><body><h1>Hi Prutha!</h1></body></html>";
+		return "<html><body><h1>Hi Prutha!</h1><p>" + studentid +  "</p></body></html>";
 	}
 
 	@POST
