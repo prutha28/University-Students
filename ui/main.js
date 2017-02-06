82b2eb25fc11006fe0c73465f19a108244fed5fa
prutha28 <prutha.bits@gmail.com>
2017-02-06 00:43:46 -0500

    Added the POST, PUT & DELETE http method calls.

--- ui/main.js
+++ ui/main.js
@@ -4,21 +4,67 @@ var $ = require("jquery");
 var request = require("request");
 
 
-var headers = {
-	'Content-Type' : 'text/html',
-	'Accept' : 'text/html'
-};
-
-
 var client = {
 
 	getStudent : function( onResponse){
+
+		var headers = {
+			'Content-Type' : 'text/html',
+			'Accept' : 'text/html'
+		};
 		// Using needle 
-		needle.get("http://localhost:8080/studentportal/webapi/students", {headers:headers}, onResponse)
+		// needle.get("http://localhost:8080/studentportal/webapi/students", {headers:headers}, onResponse)
 		// using jquery
 		// using request
-		request("http://localhost:8080/studentportal/webapi/students", onResponse);
+		// request("http://localhost:8080/studentportal/webapi/students", onResponse);
 
+	},
+
+	addStudent : function( onResponse){
+		
+		var headers = {
+			'Content-Type' : 'application/json',
+			'Accept' : 'text/html'
+		};
+		
+		var data = {
+			'name' : 'prutha'
+		};
+
+		// Using needle 
+		needle.post("http://localhost:8080/studentportal/webapi/students", data, {headers:headers}, onResponse)
+	},
+
+	
+	updateStudent : function( onResponse){
+
+		var headers = {
+			'Content-Type' : 'application/json',
+			'Accept' : 'text/html'
+		};
+
+		var data = {
+			'name' : 'prutha'
+		};
+		
+		// Using needle 
+		// needle.put("http://localhost:8080/studentportal/webapi/students", data, {headers:headers}, onResponse)
+	},
+
+	
+	deleteStudent : function( onResponse){
+		
+		var headers = {
+			'Content-Type' : 'application/json',
+			'Accept' : 'text/html'
+		};
+		
+		var data = {
+			'name' : 'prutha'
+		};
+		
+		// Using needle 
+		// needle.delete("http://localhost:8080/studentportal/webapi/students", data, {headers:headers}, onResponse)
 	}
 
 };
@@ -32,5 +78,31 @@ client.getStudent(function(error, response){
     if (!error && response.statusCode == 200) {
     	console.log(response);
   	}
-
 }); // Get Student
+
+
+
+client.addStudent(function(error, response){    
+
+    if (!error && response.statusCode == 200) {
+    	console.log(response);
+  	}
+}); // Add Student
+
+
+
+client.updateStudent(function(error, response){    
+
+    if (!error && response.statusCode == 200) {
+    	console.log(response);
+  	}
+}); // Update Student
+
+
+
+client.deleteStudent(function(error, response){    
+    
+    if (!error && response.statusCode == 200) {
+    	console.log(response);
+  	}
+}); // Delete Student
