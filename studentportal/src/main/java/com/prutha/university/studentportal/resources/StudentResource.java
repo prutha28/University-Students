41b0a1d4736d0bc360b184dc4631fc65c6d18dec
prutha28 <prutha.bits@gmail.com>
2017-02-06 01:26:27 -0500

    Adds appropriate paths and paramaters to GET, PUT, POST & DELETE.

--- studentportal/src/main/java/com/prutha/university/studentportal/resources/StudentResource.java
+++ studentportal/src/main/java/com/prutha/university/studentportal/resources/StudentResource.java
@@ -10,37 +10,50 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.prutha.university.studentportal.entities.Address;
+import com.prutha.university.studentportal.entities.Student;
+
 @Path("/students")
 public class StudentResource {
 	
 	@GET
 	@Produces(MediaType.TEXT_HTML)
+	public String getAllStudents(
+			@PathParam("studentId") String studentid){
+		return "<html><body><h1>Hi Students!</h1><p>" + studentid +  "</p></body></html>";
+	}
+	
+	
+	@GET
+	@Produces(MediaType.TEXT_HTML)
 	@Path("/{studentId}")
-	public String getStudent(@PathParam("studentId") 
-							String studentid){
-//		ResponseBuilder rb = Response.accepted();
-//		return rb.build();
-		return "<html><body><h1>Hi Prutha!</h1><p>" + studentid +  "</p></body></html>";
+	public String getStudent(
+			@PathParam("studentId") String studentid){
+		return "<html><body><h1>Hi student!</h1><p>" + studentid +  "</p></body></html>";
 	}
 
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_HTML)
 	public String addStudent(){
-		return "A new Student added successfully.";
+		return "<html><body><h1>Hi Prutha!</h1><p>";
 	}
 	
 	@PUT
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_HTML)
-	public String updateStudent(){
-		return "Student details updated successfully.";
+	@Path("/{studentId}")
+	public String updateStudent(
+			@PathParam("studentId") String studentid){
+		return "StudentDO details updated successfully.";
 	}
 	
 	@DELETE
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.TEXT_HTML)
-	public String deleteStudent(){
-		return "Student deleted successfully.";
+	@Path("/{studentId}")
+	public String deleteStudent(
+			@PathParam("studentId") String studentid){
+		return "StudentDO deleted successfully.";
 	}
 }
