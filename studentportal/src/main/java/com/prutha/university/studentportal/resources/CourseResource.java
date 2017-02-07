f0e8f7df634c5a1e62aea09a8092a7e08fc35d55
prutha28 <prutha.bits@gmail.com>
2017-02-06 19:23:47 -0500

    Adds the Basic Method signatures for GET, PUT, POST & DELETE.

--- studentportal/src/main/java/com/prutha/university/studentportal/resources/CourseResource.java
+++ studentportal/src/main/java/com/prutha/university/studentportal/resources/CourseResource.java
@@ -1,6 +1,47 @@
 package com.prutha.university.studentportal.resources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+@Path("/courses")
 public class CourseResource {
 
+	@GET
+	public String getAllCourses(){
+		return "All courses";
+	}
+	
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.TEXT_HTML)
+	public String addCourse(){
+		return "Course Added";
+	}
+	
+	@GET
+	@Path("/{courseId}")
+	@Produces(MediaType.TEXT_HTML)
+	public String getCourse( String courseId){
+		return "Getting course & its details";
+	}
+	
+	@PUT
+	@Path("/{courseId}")
+	@Produces(MediaType.TEXT_HTML)
+	public String updateCourse( String courseId){
+		return "Updating course details";
+	}
+	
+	@DELETE
+	@Path("/{courseId}")
+	@Produces(MediaType.TEXT_HTML)
+	public String deleteCourse( String courseId){
+		return "Deleting a course";
+	}
 }
