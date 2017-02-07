e644fcf1836486ae468c224079b9d49139512ba5
prutha28 <prutha.bits@gmail.com>
2017-02-06 19:46:33 -0500

    Adds the Basic Method signatures for GET, PUT, POST & DELETE.

--- studentportal/src/main/java/com/prutha/university/studentportal/resources/FacultyResource.java
+++ studentportal/src/main/java/com/prutha/university/studentportal/resources/FacultyResource.java
@@ -1,6 +1,51 @@
 package com.prutha.university.studentportal.resources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
+@Path("/faculties")
 public class FacultyResource {
 
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String getAllFaculties(){
+		return "All Faculties";
+	}
+	
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.TEXT_HTML)
+	public String addFaculty(){
+		return "A new Faculty Added";
+	}
+	
+	@GET
+	@Path("/{facultyId}")
+	@Produces(MediaType.TEXT_HTML)
+	public String getFaculty( String facultyId){
+		return "Getting Faculty & its details";
+	}
+	
+	@PUT
+	@Path("/{facultyId}")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.TEXT_HTML)
+	public String updateFaculty( String facultyId){
+		return "Updating Faculty details";
+	}
+	
+	@DELETE
+	@Path("/{facultyId}")
+	@Produces(MediaType.TEXT_HTML)
+	@Consumes(MediaType.TEXT_HTML)
+	public String deleteFaculty( String facultyId){
+		return "Deleting a Faculty from the system";
+	}
+	
 }
