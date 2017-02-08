c4e54b5fdbaa5c7b12ce8831b573cfda2fd227fc
prutha28 <prutha.bits@gmail.com>
2017-02-08 01:27:38 -0500

    Adds the Message Model class and annotates it with XmlRootElement.

---
+++ studentportal/src/main/java/com/prutha/university/studentportal/model/Message.java
@@ -0,0 +1,49 @@
+package com.prutha.university.studentportal.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Message {
+
+	private int id;
+	private String message;
+	private String author;
+	
+	/**
+	 * No Args Constructor
+	 */
+	public Message(){
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Message(int id, String message, String author) {
+		super();
+		this.id = id;
+		this.message = message;
+		this.author = author;
+	}
+
+}
