9ba4602c0864b8db54098d0aa3fd81ecf047f6e4
prutha28 <prutha.bits@gmail.com>
2017-02-10 01:52:39 -0500

    Created Faculty class.

---
+++ studentportal/src/main/java/com/prutha/university/studentportal/model/Faculty.java
@@ -0,0 +1,25 @@
+package com.prutha.university.studentportal.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Faculty {
+
+	private String professorId;
+	private String designation;
+	
+	public String getProfessorId() {
+		return professorId;
+	}
+	public void setProfessorId(String professorId) {
+		this.professorId = professorId;
+	}
+	public String getDesignation() {
+		return designation;
+	}
+	public void setDesignation(String designation) {
+		this.designation = designation;
+	}
+	
+	
+}
