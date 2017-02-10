5ade628f696da0bbd64960e45dadca0c74ba57c4
prutha28 <prutha.bits@gmail.com>
2017-02-10 01:51:08 -0500

    Creating Map objects to hold the mock data.

--- studentportal/src/main/java/com/prutha/university/studentportal/database/Database.java
+++ studentportal/src/main/java/com/prutha/university/studentportal/database/Database.java
@@ -3,8 +3,17 @@ package com.prutha.university.studentportal.database;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.prutha.university.studentportal.model.Address;
+import com.prutha.university.studentportal.model.Course;
+import com.prutha.university.studentportal.model.Email;
+import com.prutha.university.studentportal.model.Faculty;
+import com.prutha.university.studentportal.model.Gradepoint;
+import com.prutha.university.studentportal.model.LectureSlot;
 import com.prutha.university.studentportal.model.Message;
+import com.prutha.university.studentportal.model.Phone;
 import com.prutha.university.studentportal.model.Profile;
+import com.prutha.university.studentportal.model.Student;
+import com.prutha.university.studentportal.resources.FacultyResource;
 
 /**
  * A Dummy database class that holds some dummy data
@@ -15,11 +24,51 @@ public class Database {
 
 	private static Map<Integer, Message> messages = new HashMap<Integer, Message>();
 	private static Map<Integer, Profile> profiles = new HashMap<Integer, Profile>();
-
+	private static Map<Integer, Student> students = new HashMap<Integer, Student>();
+	private static Map<Integer, Course> courses = new HashMap<Integer, Course>();
+	private static Map<Integer, Faculty> faculties = new HashMap<Integer, Faculty>();
+	private static Map<Integer, Address> addresses = new HashMap<Integer, Address>();
+	private static Map<Integer, Phone> phones = new HashMap<Integer, Phone>();
+	private static Map<Integer, Email> emails = new HashMap<Integer, Email>();
+	private static Map<Integer, Gradepoint> grades = new HashMap<Integer, Gradepoint>();
+	private static Map<Integer, LectureSlot> schedules = new HashMap<Integer, LectureSlot>();
+	
 	public static Map<Integer, Message> getMessages() {
 		return messages;
 	}
 	
+	public static Map<Integer, Student> getStudents() {
+		return students;
+	}
+
+	public static Map<Integer, Course> getCourses() {
+		return courses;
+	}
+
+	public static Map<Integer, Faculty> getFaculties() {
+		return faculties;
+	}
+
+	public static Map<Integer, Address> getAddresses() {
+		return addresses;
+	}
+
+	public static Map<Integer, Phone> getPhones() {
+		return phones;
+	}
+
+	public static Map<Integer, Email> getEmails() {
+		return emails;
+	}
+
+	public static Map<Integer, Gradepoint> getGrades() {
+		return grades;
+	}
+
+	public static Map<Integer, LectureSlot> getSchedules() {
+		return schedules;
+	}
+
 	public static Map<Integer, Profile> getProfiles() {
 		return profiles;
 	}
