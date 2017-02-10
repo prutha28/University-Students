5ae6b395ddf7992b47788359e5edb4b696224b37
prutha28 <prutha.bits@gmail.com>
2017-02-10 03:15:53 -0500

    Adds new fields to Faculty.

--- studentportal/src/main/java/com/prutha/university/studentportal/model/Faculty.java
+++ studentportal/src/main/java/com/prutha/university/studentportal/model/Faculty.java
@@ -7,16 +7,37 @@ public class Faculty {
 
 	private String professorId;
 	private String designation;
+	private String officeAddress;
+	private String officeHours;
 	
 	public Faculty(){
 		
 	}
 	
-	public Faculty(String professorId, String designation) {
+	public Faculty(String professorId, String designation, String officeAddress, String officeHours) {
 		super();
 		this.professorId = professorId;
 		this.designation = designation;
+		this.officeAddress = officeAddress;
+		this.officeHours = officeHours;
 	}
+	
+	public String getOfficeAddress() {
+		return officeAddress;
+	}
+
+	public void setOfficeAddress(String officeAddress) {
+		this.officeAddress = officeAddress;
+	}
+
+	public String getOfficeHours() {
+		return officeHours;
+	}
+
+	public void setOfficeHours(String officeHours) {
+		this.officeHours = officeHours;
+	}
+
 	public String getProfessorId() {
 		return professorId;
 	}
